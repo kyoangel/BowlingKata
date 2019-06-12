9e26616bed7552212d11bff33e293480f15b8fc7
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2019-06-13 06:24:43 +0800

    finished

---
+++ BowlingKata/Bowling.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BowlingKata
+{
+	public class Bowling
+	{
+		private readonly int[] _rolls = new int[21];
+		private int _idxOfNextFrame;
+		private int _rollIndex;
+
+		public void Roll(int pins)
+		{
+			_rolls[_rollIndex++] = pins;
+		}
+
+		private IEnumerable<int> CalculateFrameScore()
+		{
+			var frames = new int[10];
+			for (var i = 0; i < frames.Length; i++)
+			{
+				if (IsSpare(_idxOfNextFrame))
+				{
+					frames[i] = SpareScore();
+					_idxOfNextFrame += 2;
+				}
+				else if (IsStrike(_idxOfNextFrame))
+				{
+					frames[i] = StrikeScore();
+					_idxOfNextFrame++;
+				}
+				else
+				{
+					frames[i] = NormalScore();
+					_idxOfNextFrame += 2;
+				}
+			}
+
+			return frames;
+		}
+
+		private int NormalScore()
+		{
+			return _rolls[_idxOfNextFrame] + _rolls[_idxOfNextFrame + 1];
+		}
+
+		private int StrikeScore()
+		{
+			return 10 + _rolls[_idxOfNextFrame + 1] + _rolls[_idxOfNextFrame + 2];
+		}
+
+		private int SpareScore()
+		{
+			return _rolls[_idxOfNextFrame] + _rolls[_idxOfNextFrame + 1] + _rolls[_idxOfNextFrame + 2];
+		}
+
+		public int Score()
+		{
+			var frames = CalculateFrameScore();
+			return frames.Sum();
+		}
+
+		private bool IsSpare(int frameIndex)
+		{
+			return _rolls[frameIndex] + _rolls[frameIndex + 1] == 10;
+		}
+
+		private bool IsStrike(int frameIndex)
+		{
+			return _rolls[frameIndex] == 10;
+		}
+	}
+}
--- BowlingKata/UnitTest1.cs
+++ BowlingKata/UnitTest1.cs
@@ -1,14 +1,91 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BowlingKata
 {
 	[TestClass]
-	public class UnitTest1
+	public class BowlingTests
 	{
+		private readonly Bowling _bowling = new Bowling();
+
 		[TestMethod]
-		public void TestMethod1()
+		public void no_roll_score_zero()
 		{
+			ScoreShouldBe(0);
+		}
+
+		[TestMethod]
+		public void roll_all_one_score_20()
+		{
+			RollMany(20, 1);
+			ScoreShouldBe(20);
+		}
+
+		[TestMethod]
+		public void roll_spare_then_one_score_12()
+		{
+			_bowling.Roll(5);
+			_bowling.Roll(5);
+			_bowling.Roll(1);
+			RollMany(17, 0);
+			ScoreShouldBe(12);
+		}
+
+		[TestMethod]
+		public void strike_then_1_score_12()
+		{
+			_bowling.Roll(10);
+			_bowling.Roll(1);
+			RollMany(18, 0);
+			ScoreShouldBe(12);
+		}
+
+		[TestMethod]
+		public void strike_then_1_then_1_score_14()
+		{
+			_bowling.Roll(10);
+			_bowling.Roll(1);
+			_bowling.Roll(1);
+			RollMany(17, 0);
+			ScoreShouldBe(14);
+		}
+
+		[TestMethod]
+		public void Suggested_test_case_all_9_0()
+		{
+			for (var i = 0; i < 10; i++)
+			{
+				_bowling.Roll(9);
+				_bowling.Roll(0);
+			}
+			ScoreShouldBe(90);
+		}
+
+		[TestMethod]
+		public void all_file_score_150()
+		{
+			RollMany(21, 5);
+			ScoreShouldBe(150);
+		}
+
+		[TestMethod]
+		public void all_strike_score_300()
+		{
+			RollMany(12, 10);
+			ScoreShouldBe(300);
+		}
+
+		private void RollMany(int rolls, int pinsPerRoll)
+		{
+			for (var i = 0; i < rolls; i++)
+			{
+				_bowling.Roll(pinsPerRoll);
+			}
+		}
+
+		private void ScoreShouldBe(int expected)
+		{
+			var score = _bowling.Score();
+			Assert.AreEqual(expected, score);
 		}
 	}
 }
