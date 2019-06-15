0ac71be24b722dbb563eb0a543c27ffa976be1c4
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2019-06-15 16:21:30 +0800

    finish sampe tese

--- BowlingKata/Bowling.cs
+++ BowlingKata/Bowling.cs
@@ -2,9 +2,91 @@
 {
 	public class Bowling
 	{
+		public int CurrentFrame
+		{
+			get => _currentFrame >= 10 ? 10 : _currentFrame;
+			set => _currentFrame = value;
+		}
+
+		private int _rollIdx;
+		private readonly int[] _rolls = new int[21];
+		private bool _firstThrow = true;
+		private int _currentFrame = 1;
+		private int _throws = 0;
+
 		public int Score()
 		{
-			return 0;
+			return ScoreOfFrame(CurrentFrame);
+		}
+
+		public void Roll(int pins)
+		{
+			AdjustCurrentFrame(pins);
+			_rolls[_rollIdx++] = pins;
+		}
+
+		private void AdjustCurrentFrame(int pins)
+		{
+			if (_firstThrow)
+			{
+				if (pins == 10)
+				{
+					_currentFrame++;
+					return;
+				}
+
+				_firstThrow = false;
+			}
+			else
+			{
+				_firstThrow = true;
+				_currentFrame++;
+			}
+		}
+
+		public int ScoreOfFrame(int theFrame)
+		{
+			var score = 0;
+			for (var current = 0; current < theFrame; current++)
+			{
+				if (IsStrike())
+				{
+					score += 10 + _rolls[_throws + 1] + _rolls[_throws + 2];
+					_throws++;
+				}
+				else if (IsSpare())
+				{
+					score += 10 + SpareBonus();
+					_throws += 2;
+				}
+				else
+				{
+					score += _rolls[_throws] + _rolls[_throws + 1];
+					_throws += 2;
+				}
+			}
+
+			return score;
+		}
+
+		private int SpareBonus()
+		{
+			return _rolls[_throws + 2];
+		}
+
+		private bool IsSpare()
+		{
+			return _rolls[_throws] + _rolls[_throws + 1] == 10;
+		}
+
+		private bool IsStrike()
+		{
+			return _rolls[_throws] == 10;
+		}
+
+		private int SpareBonus(int ball)
+		{
+			return _rolls[ball + 2];
 		}
 	}
 }
--- BowlingKata/UnitTest1.cs
+++ BowlingKata/UnitTest1.cs
@@ -5,12 +5,131 @@ namespace BowlingKata
 	[TestClass]
 	public class BowlingTests
 	{
+		private Bowling _bowling = new Bowling();
+
 		[TestMethod]
 		public void no_roll_score_0()
 		{
-			var bowling = new Bowling();
-			var score = bowling.Score();
-			Assert.AreEqual(0, score);
+			ScoreShouldBe(0);
+		}
+
+		[TestMethod]
+		public void roll_1_score_1()
+		{
+			_bowling.Roll(1);
+			TheResultShouldBe(1, 1, 1);
+		}
+
+		[TestMethod]
+		public void roll_111_score_3()
+		{
+			RollMany(3, 1);
+			TheResultShouldBe(2, 2, 3);
+		}
+
+		[TestMethod]
+		public void roll_spare_then_5_score_20()
+		{
+			RollMany(3, 5);
+			TheResultShouldBe(2, 15, 20);
+		}
+
+		[TestMethod]
+		public void roll_strike_then_5_score_20()
+		{
+			_bowling.Roll(10);
+			_bowling.Roll(5);
+			TheResultShouldBe(2, 15, 20);
+		}
+
+		[TestMethod]
+		public void roll_strike_then_0_5_score_20()
+		{
+			_bowling.Roll(10);
+			_bowling.Roll(0);
+			_bowling.Roll(5);
+			TheResultShouldBe(3, 15, 20);
+		}
+
+		[TestMethod]
+		public void roll_all_spare_score_150()
+		{
+			RollMany(21, 5);
+			TheResultShouldBe(10, 15, 150);
+		}
+
+		[TestMethod]
+		public void roll_all_strike_score_300()
+		{
+			RollMany(12, 10);
+			TheResultShouldBe(10, 30, 300);
+		}
+
+		[TestMethod]
+		public void roll_all_9_0_score_90()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				_bowling.Roll(9);
+				_bowling.Roll(0);
+			}
+			TheResultShouldBe(10, 9, 90);
+		}
+
+		[TestMethod]
+		public void Sample_All_cases()
+		{
+			_bowling.Roll(1);
+			_bowling.Roll(4);
+			_bowling.Roll(4);
+			_bowling.Roll(5);
+			_bowling.Roll(6);
+			_bowling.Roll(4);
+			_bowling.Roll(5);
+			_bowling.Roll(5);
+			_bowling.Roll(10);
+			_bowling.Roll(0);
+			_bowling.Roll(1);
+			_bowling.Roll(7);
+			_bowling.Roll(3);
+			_bowling.Roll(6);
+			_bowling.Roll(4);
+			_bowling.Roll(10);
+			_bowling.Roll(2);
+			_bowling.Roll(8);
+			_bowling.Roll(6);
+			TheResultShouldBe(10, 5, 133);
+		}
+
+		private void TheResultShouldBe(int currentFrame, int firstFrameScore, int totalScore)
+		{
+			CurrentFrameShouldBe(currentFrame);
+			FirstFrameScoreShouldBe(firstFrameScore);
+			ScoreShouldBe(totalScore);
+		}
+
+		private void RollMany(int rolls, int pinsPerRoll)
+		{
+			for (int i = 0; i < rolls; i++)
+			{
+				_bowling.Roll(pinsPerRoll);
+			}
+		}
+
+		private void CurrentFrameShouldBe(int expected)
+		{
+			Assert.AreEqual(expected, _bowling.CurrentFrame);
+		}
+
+		private void FirstFrameScoreShouldBe(int expected)
+		{
+			Assert.AreEqual(expected, _bowling.ScoreOfFrame(1));
+		}
+
+		private void ScoreShouldBe(int expected)
+		{
+			var score = _bowling.Score();
+			Assert.AreEqual(expected, score);
 		}
 	}
 }
